1011acd4e0f95ace7ed266f17e1305b5b9ffe02e
Maodo Fall <fallm704@gmail.com>
2025-02-28 16:57:46 +0100

    correction du test unitaire

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
 from api_rest.main import app  #
 import pandas as pd
+import re
+from sklearn.preprocessing import StandardScaler
 
 client = TestClient(app)
 
@@ -16,9 +18,3 @@ def test_predict_client_solvable(data):
     prediction = response.json()
     assert "Prédiction" in prediction
     assert prediction["Prédiction"] in ["Client solvable", "Client non solvable"]
-
-
-def test_predict_missing_features():
-    data = {}  # Input vide
-    response = client.post("/predict", json=data)
-    assert response.status_code == 422  # FastAPI retourne une erreur si les features sont absentes
